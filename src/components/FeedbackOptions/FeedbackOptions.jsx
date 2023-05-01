4023b7ea59ce8f79d152546191b759a74b1438aa
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-01 19:52:28 +0300

    Update FeedbackOptions.jsx

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,1 +1,20 @@
 import PropTypes from 'prop-types';
+
+export const FeedbackOptions = () => {
+  return (
+    <form>
+      <label>
+        <input type="radio" name="feedback" value="good" />
+        Good
+      </label>
+      <label>
+        <input type="radio" name="feedback" value="neutral" />
+        Neutral
+      </label>
+      <label>
+        <input type="radio" name="feedback" value="bad" />
+        Bad
+      </label>
+    </form>
+  );
+};

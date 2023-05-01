19b7a773e5d62fada0be1679e99c5abb6b01d947
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-01 20:32:14 +0300

    Create App.styled.jsx

---
+++ src/components/App.styled.jsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  width: 100%;
+`;

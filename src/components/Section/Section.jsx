77425232ea1dfee43dca90d1a32ce2db3a031458
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-01 22:32:18 +0300

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,13 +2,15 @@ import PropTypes from 'prop-types';
 import { Wrap } from './Section.styled';
 
 export const Section = ({ title, children }) => {
-  <Wrap>
-    <h2>{title}</h2>
-    {children}
-  </Wrap>;
+  return (
+    <Wrap>
+      <h2>{title}</h2>
+      {children}
+    </Wrap>
+  );
 };
 
 Section.propTypes = {
-    title: PropTypes.string,
-    children: PropTypes.node,
+  title: PropTypes.string,
+  children: PropTypes.node,
 };

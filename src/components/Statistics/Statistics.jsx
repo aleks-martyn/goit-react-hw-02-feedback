1bae2a22263f25217b8c2e491f31dba5a2e7b539
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-01 23:32:20 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,1 +1,11 @@
 //import PropTypes from 'prop-types';
+
+export const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  );
+};

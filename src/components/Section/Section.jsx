652b40dd838ce3c838ca7ecf2e759fc728bf53cd
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-01 22:02:24 +0300

    Create Section.jsx

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import { Wrap } from './Section.styled';
+
+export const Section = ({ title, children }) => {
+  <Wrap>
+    <h2>{title}</h2>
+    {children}
+  </Wrap>;
+};
+
+Section.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.node,
+};

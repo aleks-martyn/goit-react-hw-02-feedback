cc672ae9bf637db4ce4b1e885be154a741078b06
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-01 23:32:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { Section } from './Section';
 import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
 import { Container } from './App.styled';
 
 export class App extends Component {
@@ -11,10 +12,14 @@ export class App extends Component {
   };
 
   render() {
+    const { good, neutral, bad } = this.state;
     return (
       <Container>
         <Section title="Please leave feedback">
           <FeedbackOptions />
+        </Section>
+        <Section title="Statistics">
+          <Statistics good={good} neutral={neutral} bad={bad} />
         </Section>
       </Container>
     );

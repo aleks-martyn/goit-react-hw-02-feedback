71d7c344bbe824f4f7b8718a30aa02f8c895b67b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-02 23:57:54 +0300

    Update FeedbackOptions.styled.jsx

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -9,9 +9,16 @@ export const Button = styled.button`
   height: 30px;
   font-weight: bold;
   text-transform: capitalize;
+  border: 1px solid grey;
+  border-radius: 4px;
+  outline: none;
   transition: transform 250ms linear;
 
   &:hover {
     transform: scale(1.1);
+  }
+
+  &:active {
+    background-color: teal;
   }
 `;

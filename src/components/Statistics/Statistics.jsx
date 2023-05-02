95fa0a0dc909480e7a16ac7c76f681584854792b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-02 18:06:44 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,20 +1,27 @@
 import PropTypes from 'prop-types';
 import { Text } from './Statistics.styled';
 
-export const Statistics = (props) => {
-  const { good, neutral, bad } = props;
-  const buttons = Object.keys(props);
-  
+export const Statistics = props => {
+  const labels = Object.keys(props);
+
   return (
     <div>
-      {buttons.map(btn => (<Text key={btn}>{btn}: {props[btn]}</Text>))}
+      {labels.map(label => (
+        <Text key={label}>
+          {label}: {props[label]}
+        </Text>
+      ))}
     </div>
   );
 };
 
 Statistics.propTypes = {
-  good: PropTypes.number,
-  neutral: PropTypes.number,
-  bad: PropTypes.number,
+  props: PropTypes.objectOf(
+    PropTypes.shape({
+      goog: PropTypes.number,
+      neutral: PropTypes.number,
+      bad: PropTypes.bad,
+      total: PropTypes.number,
+    })
+  ),
 };
-

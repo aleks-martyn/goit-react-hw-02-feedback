613eac8026a8174942d7970598a93e6ce4c3c184
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-02 20:21:26 +0300

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
 import { Text } from './Statistics.styled';
 
-export const Statistics = props => {
-  const labels = Object.keys(props);
-
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
   return (
     <div>
-      {labels.map(label => (
-        <Text key={label}>
-          {label}: {props[label]}
-        </Text>
-      ))}
+      <Text>Good: {good}</Text>
+      <Text>Neutral: {neutral}</Text>
+      <Text>Bad: {bad}</Text>
+      <Text>Total: {total}</Text>
+      <Text>Positive feedback: {positivePercentage}%</Text>
     </div>
   );
 };
 
 Statistics.propTypes = {
-  props: PropTypes.objectOf(
-    PropTypes.shape({
-      good: PropTypes.number,
-      neutral: PropTypes.number,
-      bad: PropTypes.number,
-      total: PropTypes.number,
-      positivePercentage: PropTypes.number,
-    })
-  ),
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
 };

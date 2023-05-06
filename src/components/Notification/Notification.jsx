6fb4439bdf83678fc2c092f6bdc6a2dded39c133
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-06 17:27:26 +0300

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { Text } from './Notification.styled';
 
-export const Notification = ({message}) => <Text>{message}</Text>;
+export const Notification = ({ message }) => <Text>{message}</Text>;
 
 Notification.propTypes = {
   message: PropTypes.string.isRequired,
